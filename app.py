f6a3d3ff8b056abdf077e9355f957f2423f6154d
Yoni Tsafir <yoni@joytunes.com>
2012-07-21 21:48:50 +0300

    Added favicon

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 # -*- coding=utf-8 -*-
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                                   'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
